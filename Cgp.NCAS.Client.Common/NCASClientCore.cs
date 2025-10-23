1b1edc8289a9487ae10aca4bcde83e3ee2e8b3ee
PeterS6910 <sorokapeter6910@gmail.com>
2025-10-23 13:23:11 +0200

    lprKamera2

--- Cgp.NCAS.Client.Common/NCASClientCore.cs
+++ Cgp.NCAS.Client.Common/NCASClientCore.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 using Contal.Cgp.Client.Common;
@@ -133,7 +133,6 @@ namespace Contal.Cgp.NCAS.Client.Common
                 _remotingProviderInterface.AttachCallbackHandler(CRUpgradeProgressChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCULookupFinishedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(AlarmTransmittersLookupFinishedHandler.Singleton);
-                _remotingProviderInterface.AttachCallbackHandler(LprCameraLookupFinishedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorValuesChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorOnlineStateChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorAlarmStateChangedHandler.Singleton);
@@ -188,7 +187,6 @@ namespace Contal.Cgp.NCAS.Client.Common
                 _remotingProviderInterface.AttachCallbackHandler(CRUpgradeProgressChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCULookupFinishedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(AlarmTransmittersLookupFinishedHandler.Singleton);
-                _remotingProviderInterface.AttachCallbackHandler(LprCameraLookupFinishedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorValuesChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorOnlineStateChangedHandler.Singleton);
                 _remotingProviderInterface.AttachCallbackHandler(CCUUpsMonitorAlarmStateChangedHandler.Singleton);
